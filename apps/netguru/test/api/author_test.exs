480a37f35256fd78407335318ec75a01663a0029
Mateusz Bielawski <mateusz.bielawski@amberbit.com>
2019-07-31 00:04:15 +0200

    Add tests for API create_author

--- apps/netguru/test/api/author_test.exs
+++ apps/netguru/test/api/author_test.exs
@@ -14,4 +14,23 @@ defmodule Netguru.API.AuthorTest do
             end
         end
     end
+
+    describe "create_author/1" do
+        @author %{
+            first_name: "name",
+            last_name: "name",
+            age: 18
+        }
+
+        test "creates a new author when the author is valid" do
+            {:ok, %Author{id: id} = author} = API.create_author(@author)
+
+            assert ^author = Repo.get(Author, id)
+        end
+
+        test "returns an error when the author is invalid" do
+            invalid_author = Map.delete(@author, :age)
+            assert {:error, %Ecto.Changeset{valid?: false, errors: errors}} = API.create_author(invalid_author)
+        end
+    end
 end
