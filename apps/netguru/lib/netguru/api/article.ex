68c0769fb1705aab908d0297c43592c3e327898f
Mateusz Bielawski <mateusz.bielawski@amberbit.com>
2019-07-31 13:43:01 +0200

    Add docs

--- apps/netguru/lib/netguru/api/article.ex
+++ apps/netguru/lib/netguru/api/article.ex
@@ -3,6 +3,8 @@ defmodule Netguru.API.Article do
     alias Netguru.Repo
 
     def get_article!(id), do: Repo.get! Article, id
+
+    @spec delete_article(id :: String.t) :: {:ok, %Article{}} | {:error, :not_found}
     def delete_article(id) do
         case Repo.get(Article, id) do
             %Article{} = article -> Repo.delete(article)
@@ -10,6 +12,7 @@ defmodule Netguru.API.Article do
         end
     end
 
+    @spec create_article(article :: map) :: {:ok, %Article{}} | {:error, %Ecto.Changeset{}}
     def create_article(%{} = article) do
         article = Map.put(article, "published_date", DateTime.utc_now)
         changeset = Article.changeset(%Article{}, article)
@@ -17,6 +20,12 @@ defmodule Netguru.API.Article do
         Repo.insert changeset
     end
 
+    @doc """
+        Returns all the articles from the db.
+
+        Possible options:
+            - preload (default false)
+    """
     def index_articles(opts \\ []) do
         preload = Keyword.get(opts, :preload, false)
 
--- apps/netguru/lib/netguru/api/author.ex
+++ apps/netguru/lib/netguru/api/author.ex
@@ -4,12 +4,14 @@ defmodule Netguru.API.Author do
 
     def get_author!(id), do: Repo.get!(Author, id)
 
+    @spec create_author(author :: map) :: {:ok, %Author{}} | {:error, %Ecto.Changeset{}}
     def create_author(%{} = author) do
         %Author{}
             |> Author.changeset(author)
             |> Repo.insert
     end
 
+    @spec update_author(id :: String.t, update_author :: map) :: {:ok, %Author{}} | {:error, %Ecto.Changeset{}} | {:error, :not_found}
     def update_author(id, %{} = updated_author) do
         case Repo.get(Author, id) do
             %Author{} = author -> author
