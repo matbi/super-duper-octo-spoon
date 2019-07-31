9919560aa7c86976bfe147db094741c98bcc8c00
Mateusz Bielawski <mateusz.bielawski@amberbit.com>
2019-07-31 12:39:28 +0200

    Add Article API tests

--- apps/netguru/test/api/article_test.exs
+++ apps/netguru/test/api/article_test.exs
@@ -16,4 +16,53 @@ defmodule Netguru.API.ArticleTest do
             assert {:error, :not_found} = API.delete_article 2
         end
     end
+
+    describe "get_article!/1" do
+        test "should return the existing article" do
+            assert %Article{id: 1} = API.get_article! 1
+        end
+
+        test "should raise when the article doesn't exist" do
+            assert_raise Ecto.NoResultsError, fn -> API.get_article! 2 end
+        end
+    end
+
+    describe "create_article/1" do
+        @article %{
+            "description" => "desc",
+            "title" => "title",
+            "author_id" => 1,
+            "body" => "body"
+        }
+
+        test "should create an article when the data is valid" do
+            assert {:ok, %Article{id: id}} = API.create_article(@article)
+            assert %Article{id: ^id} = API.get_article!(id)
+        end
+
+        test "should return an error when the data is invalid" do
+            invalid_article = Map.delete(@article, "body")
+            assert {:error, %Ecto.Changeset{valid?: false}} = API.create_article(invalid_article)
+        end
+    end
+
+    describe "index_articles/1" do
+        test "should return the whole list of articles" do
+            assert length(Repo.all(Article)) == length(API.index_articles())
+        end
+
+        test "shouldn't preload the author when not stated explicitly" do
+            articles = API.index_articles()
+            article = List.first(articles)
+
+            assert %{author: %Ecto.Association.NotLoaded{}} = article
+        end
+
+        test "should preload the author when stated explicitly" do
+            articles = API.index_articles(preload: true)
+            article = List.first(articles)
+
+            assert %{author: %{id: 1}} = article
+        end
+    end
 end
